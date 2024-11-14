e571a39ed5a7fa3216ed82ed74a7d5553714e625
Simantak Dabhade <dsimantak@gmail.com>
2024-11-15 01:35:00 +0800

    Update formula with correct URL and SHA256

--- Formula/brwne.rb
+++ Formula/brwne.rb
@@ -4,7 +4,7 @@ class Brwne < Formula
     desc "brwne description"
     homepage "https://github.com/The-Pirateship/brwne"
     url "https://github.com/The-Pirateship/brwne_CLI/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "ea9b4e33cbf31646222f9ca42088811be072dbd5a4d60d88e3916b77eb7b9f36"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license :cannot_represent
   
     depends_on "python@3.13"
