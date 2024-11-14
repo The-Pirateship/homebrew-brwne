eb1095e53b23b341454cc82d3cd7a06997edbf26
Simantak Dabhade <dsimantak@gmail.com>
2024-11-15 01:25:35 +0800

    Add brwne formula

--- Formula/brwne.rb
+++ Formula/brwne.rb
@@ -1,11 +1,11 @@
-class brwne < Formula
+class Brwne < Formula
     include Language::Python::Virtualenv
   
     desc "brwne description"
-    homepage "https://github.com/The-Pirateship/brwne_CLI"
-    url "https://github.com/yourusername/yourtool/archive/refs/tags/v1.0.0.tar.gz"
+    homepage "https://github.com/The-Pirateship/brwne"
+    url "https://github.com/The-Pirateship/brwne/archive/refs/tags/v1.0.0.tar.gz"
     sha256 "ea9b4e33cbf31646222f9ca42088811be072dbd5a4d60d88e3916b77eb7b9f36"
-    license "cannot_represent"
+    license :cannot_represent
   
     depends_on "python@3.13"
   
