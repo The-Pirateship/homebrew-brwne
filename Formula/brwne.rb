fa90a6385c7df8bfd56e71961f2b65f3027b6816
Rahul <mallavarapurahul18@gmail.com>
2025-06-17 13:36:03 +0800

    updated fromula for br prod

--- Formula/brwne.rb
+++ Formula/brwne.rb
@@ -1,15 +1,15 @@
 class Brwne < Formula
   desc "Brwne CLI for internal developer tools"
   homepage "https://brwne.net"
-  url "https://github.com/The-Pirateship/homebrew-brwne/releases/download/v0.0.1/brd-darwin-amd64.tar.gz"
-  sha256 "a52919406e21a94dfe37f96931e37bc5f0e361872f60798c21391c1ac36c307a"
+  url "https://github.com/The-Pirateship/homebrew-brwne/releases/download/v0.0.1/br-darwin-amd64.tar.gz"
+  sha256 "ad4bf57f8c08a4c289d78dfce8cf5ffe9703a85579e2e630ba3e3064065f6bd6"
   version "0.0.1"
 
   def install
-    bin.install "brd"
+    bin.install "br"
   end
 
   test do
-    system "#{bin}/brd"
+    system "#{bin}/br" "--version"
   end
 end
