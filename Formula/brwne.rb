a30faef888618b86f32ec287150c6d095ab4ba57
Rahul Mallavarapu <98007910+rahulmllvrp@users.noreply.github.com>
2025-07-01 20:57:24 +0800

    Update brwne.rb

--- Formula/brwne.rb
+++ Formula/brwne.rb
@@ -1,15 +1,28 @@
 class Brwne < Formula
   desc "Brwne CLI for internal developer tools"
   homepage "https://brwne.net"
-  url "https://github.com/The-Pirateship/brwne/releases/download/v0.0.2/brwne-darwin-arm64.tar.gz"
-  sha256 "ad4bf57f8c08a4c289d78dfce8cf5ffe9703a85579e2e630ba3e3064065f6bd6"
-  version "0.0.1"
+  version "..." # TODO: set by CI
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/The-Pirateship/brwne/releases/download/v.../br-darwin-arm64.tar.gz"
+      sha256 "..." # TODO: from br-darwin-arm64.tar.gz.sha256
+    else
+      url "https://github.com/The-Pirateship/brwne/releases/download/v.../br-darwin-amd64.tar.gz"
+      sha256 "..." # TODO: from br-darwin-amd64.tar.gz.sha256
+    end
+  end
+
+  on_linux do
+    url "https://github.com/The-Pirateship/brwne/releases/download/v.../br-linux-amd64.tar.gz"
+    sha256 "..." # TODO: from br-linux-amd64.tar.gz.sha256
+  end
 
   def install
     bin.install "br"
   end
 
   test do
-    system "#{bin}/brwne", "--version"
+    system "#{bin}/br", "--version"
   end
 end
