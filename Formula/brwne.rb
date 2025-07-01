28ec13068613c0e291701054814861c077267f23
Rahul Mallavarapu <98007910+rahulmllvrp@users.noreply.github.com>
2025-07-01 20:44:49 +0800

    Update brwne.rb

--- Formula/brwne.rb
+++ Formula/brwne.rb
@@ -1,15 +1,15 @@
 class Brwne < Formula
   desc "Brwne CLI for internal developer tools"
   homepage "https://brwne.net"
-  url "https://github.com/The-Pirateship/homebrew-brwne/releases/download/v0.0.1/br-darwin-amd64.tar.gz"
+  url "https://github.com/The-Pirateship/brwne/releases/download/v0.0.2/brwne-darwin-arm64.tar.gz"
   sha256 "ad4bf57f8c08a4c289d78dfce8cf5ffe9703a85579e2e630ba3e3064065f6bd6"
   version "0.0.1"
 
   def install
-    bin.install "br"
+    bin.install "brwne"
   end
 
   test do
-    system "#{bin}/br"
+    system "#{bin}/brwne", "--version"
   end
 end
